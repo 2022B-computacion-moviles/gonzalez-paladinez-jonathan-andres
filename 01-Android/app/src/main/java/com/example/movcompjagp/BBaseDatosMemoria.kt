14e8068a18124eccab0bec647946f3fa00e874b6
Jonathan Andrés González <jonathan.gonzalez01@epn.edu.ec>
2023-01-08 23:13:25 -0500

    List View

---
+++ 01-Android/app/src/main/java/com/example/movcompjagp/BBaseDatosMemoria.kt
@@ -0,0 +1,23 @@
+package com.example.movcompjagp
+
+class BBaseDatosMemoria {
+
+    companion object{
+        val arregloBEntrenador = arrayListOf<BEntrenador>()
+
+        init {
+            arregloBEntrenador
+                .add(
+                    BEntrenador("Adrian", "a@a.com")
+                )
+            arregloBEntrenador
+                .add(
+                    BEntrenador("Vicente", "b@b.com")
+                )
+            arregloBEntrenador
+                .add(
+                    BEntrenador("Carolina", "c@c.com")
+                )
+        }
+    }
+}
---
+++ 01-Android/app/src/main/java/com/example/movcompjagp/BEntrenador.kt
@@ -0,0 +1,8 @@
+package com.example.movcompjagp
+
+class BEntrenador (
+    var nombre:String,
+    var descripcion:String
+        ){
+
+}
